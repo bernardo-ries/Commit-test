ae599fc3b3e273f8c5d8fe7dd17553181a028e8d
bernardo-ries <bernardories@gmail.com>
2026-04-01 14:40:06 -0300

    Fix activity 04: remove unnecessary files

--- ex1.lua
+++
@@ -1,9 +0,0 @@
-local nome = "Bernardo"
-local idade = 22
-local programador = true
-local nulo = nil
-
-print(nome, type(nome))
-print(idade, type(idade))
-print(programador, type(programador))
-print(nulo, type(nulo))
